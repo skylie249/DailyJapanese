6269299d34063760413446019852c0b675157d13
skylie <skylie@happyict.co.kr>
2026-07-24 17:24:26 +0900

    forth commit

--- vite.config.js
+++ vite.config.js
@@ -18,13 +18,13 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: 'favicon.svg',
+            src: 'Gemini_Generated_Image_192.svg',
             sizes: '192x192',
             type: 'image/svg+xml',
             purpose: 'any maskable'
           },
           {
-            src: 'favicon.svg',
+            src: 'Gemini_Generated_Image_512.svg',
             sizes: '512x512',
             type: 'image/svg+xml',
             purpose: 'any maskable'
